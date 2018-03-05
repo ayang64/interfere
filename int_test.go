58ea064be05e62e52f49d0eea91983555a008bd4
Ayan George <ayan@ayan.net>
2018-03-05 17:27:38 -0500

    Made goroutine increments a power of two (for the most part).

--- int_test.go
+++ int_test.go
@@ -10,17 +10,18 @@ func BenchmarkCompute(b *testing.B) {
 		Width      int
 		Height     int
 	}{
-		{Name: "1 goroutines", Points: 10, Width: 100, Height: 100, GoRoutines: 1},
-		{Name: "2 goroutines", Points: 10, Width: 100, Height: 100, GoRoutines: 2},
-		{Name: "3 goroutines", Points: 10, Width: 100, Height: 100, GoRoutines: 3},
-		{Name: "4 goroutines", Points: 10, Width: 100, Height: 100, GoRoutines: 4},
-		{Name: "8 goroutines", Points: 10, Width: 100, Height: 100, GoRoutines: 8},
-		{Name: "16 goroutine", Points: 10, Width: 100, Height: 100, GoRoutines: 16},
-		{Name: "32 goroutines", Points: 10, Width: 100, Height: 100, GoRoutines: 32},
-		{Name: "50 goroutines", Points: 10, Width: 100, Height: 100, GoRoutines: 50},
-		{Name: "64 goroutines", Points: 10, Width: 100, Height: 100, GoRoutines: 64},
-		{Name: "100 goroutines", Points: 10, Width: 100, Height: 100, GoRoutines: 100},
-		{Name: "500 goroutines", Points: 10, Width: 100, Height: 100, GoRoutines: 500},
+		{Name: "001 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 1},
+		{Name: "002 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 2},
+		{Name: "003 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 3},
+		{Name: "004 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 4},
+		{Name: "008 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 8},
+		{Name: "016 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 16},
+		{Name: "032 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 32},
+		{Name: "050 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 50},
+		{Name: "064 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 64},
+		{Name: "128 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 128},
+		{Name: "256 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 256},
+		{Name: "512 goroutines", Points: 10, Width: 512, Height: 512, GoRoutines: 512},
 	}
 
 	for _, bm := range benchmark {
